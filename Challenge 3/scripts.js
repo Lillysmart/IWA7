71d9575f912624a37fba8d63671cbc28a2f82c39
Lillysmart <137174363+Lillysmart@users.noreply.github.com>
2023-08-11 14:24:37 +0200

    final commit

--- Challenge 3/scripts.js
+++ Challenge 3/scripts.js
@@ -9,11 +9,20 @@ const sarahBalance = '-4582.2'
 const divider = '----------------------------------'
 
 // Only change below this line
+const leoNewBalance= Math.abs (parseInt(leoBalance)) // make the balance positive
+const sarahNewBalance = Math.abs (parseFloat(sarahBalance))
 
-const owed = parseInt('R' + leoBalance + sarahBalance)
-const leo = "{leoName} + {leoSurname} + \"Owed\" + \"R\" + {sarahBalance}"
-const sarah = "{leoName} + {surname} + \"Owed\" + \"R\" + {sarahBalance}"
-const total = "Total amount owed: "
-const result = leo + sarah + divider + divider + total + owed + divider
+const owed=((leoNewBalance +sarahNewBalance).toFixed(2))
+
+const leo = `${leoName}  ${leoSurname.trim() } (Owed: R ${(leoNewBalance.toFixed(2))})\n`
+
+const sarah = `${sarahName.trim()} ${sarahSurname} (Owed: R ${sarahNewBalance.toFixed(2)})\n`;
+
+const total = "   Total amount owed: " 
+ const result =`${leo}${sarah}\n${divider}\n${total}R ${owed}\n${divider}`;
 
 console.log(result)
+
+
+
+
